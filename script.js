1bac62e5b9df012d8e4d3013dedb061a182381ce
Saidur289 <saidur.riaz1@gmailcomm>
2025-11-01 19:26:17 +0600

    basic of function and object in typescripot

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('riaz');
+"use strict";
+// // console.log('riaz');
+// // let playName = 'saidur';
+// // console.log(playName);
+// // playName = 34;  give me error 
+// let playName;
+// playName = "mehedi";
+// playName = 55;
+// console.log(playName);
+function multiply(a, b) {
+    return a * b;
+}
+// console.log(multiply(4, "5")); give me error 
+console.log(multiply(4, 5));
--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
-console.log('riaz');
+// // console.log('riaz');
+// // let playName = 'saidur';
+// // console.log(playName);
+// // playName = 34;  give me error 
+// let playName;
+// playName = "mehedi";
+// playName = 55;
+// console.log(playName);
+function multiply(a: number, b:number) {
+    return a * b;
+}
+// console.log(multiply(4, "5")); give me error 
+console.log(multiply(4, 5));
